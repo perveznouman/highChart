5084280b883dadbe91a2ae544b0ec541e3bd08a4
Nouman Pervez <nouman.pervez@bighand.com>
2023-09-19 15:03:18 +0530

    Adding ChartOption class

---
+++ HighChart/Model/HOption.swift
@@ -0,0 +1,22 @@
+//
+//  HOption.swift
+//  HighChart
+//
+//  Created by Nouman Pervez on 03/08/23.
+//
+
+import Foundation
+import Highcharts
+
+class ChartOption {
+    
+    public func getOption (chartData:ChartData) -> HIOptions {
+        
+        let options = HIOptions()
+        options.yAxis = [chartData.generateYAxis()]
+        options.xAxis = [chartData.generateXAxis()]
+        options.series = [chartData.generateHSeries(name: "Cappuccino"), chartData.generateHSeries(name: "Latte"), chartData.generateHSeries(name: "Espresso"), chartData.generateHSeries(name: "Green Tea"), chartData.generateHSeries(name: "Ginger Tea")]
+        return options
+
+    }
+}
